ce978b9dc33d53607d50dfaf0212bfa85c2aaac9
Rafay Qayyum <49898477+Rafayqayyum@users.noreply.github.com>
2022-08-05 20:23:44 +0500

    Update helper_functions.py

--- helper_functions.py
+++ helper_functions.py
@@ -57,7 +57,7 @@ def make_confusion_matrix(y_true, y_pred, classes=None, figsize=(10, 10), text_s
          yticks=np.arange(n_classes), 
          xticklabels=labels, # axes will labeled with class names (if they exist) or ints
          yticklabels=labels)
-  ax.title("Confusion Matrix",fontsize=30)
+  plt.title("Confusion Matrix",fontsize=30)
   
   # Make x-axis labels appear on bottom
   ax.xaxis.set_label_position("bottom")
